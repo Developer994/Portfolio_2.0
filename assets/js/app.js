c35a2c07380742da46be43aec5b20c4fe44f41af
Silent Dev <rez_a@outlook.com>
2020-07-29 11:34:22 -0400

    Created the hamburger menu for mobile devices.

--- assets/js/app.js
+++ assets/js/app.js
@@ -37,3 +37,38 @@ $('#btn4').on("click", function () {
     $('#myProjContainer').hide();
     $('#contactContainer').show();
 })
+
+
+// Below are the queries for desktop site, 
+// as all the btn ids won't work on desktop.
+$('.btn__desktop1').on("click", function () {
+    $('#header').html(" About Me <br> <br>");
+    $('#abtMeContainer').show();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').hide();
+    $('#contactContainer').hide();
+});
+
+$('.btn__desktop2').on("click", function () {
+    $('#header').html(" My Experience <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').show();
+    $('#myProjContainer').hide();
+    $('#contactContainer').hide();
+});
+
+$('.btn__desktop3').on("click", function () {
+    $('#header').html(" My Projects <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').show();
+    $('#contactContainer').hide();
+})
+
+$('.btn__desktop4').on("click", function () {
+    $('#header').html(" Contact <br> <br>")
+    $('#abtMeContainer').hide();
+    $('#myExpContainer').hide();
+    $('#myProjContainer').hide();
+    $('#contactContainer').show();
+});
---
+++ assets/js/nav.js
@@ -0,0 +1,3 @@
+$('#btn2').on("click", function () {
+    $()
+})
